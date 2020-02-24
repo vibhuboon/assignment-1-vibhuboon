bc881ffb1647b6b282517d63071db6c09ca191e1
Yash Srivastava <yashsrivastava852000>
2020-02-24 22:14:44 +0530

    final comit

--- src/myMain/MyMain.java
+++ src/myMain/MyMain.java
@@ -9,7 +9,7 @@ public class MyMain {
     public static void main(String[] args) {
         MyList obj=new MyList();
         Scanner sc =new Scanner(System.in);
-        System.out.println("Welcome to Yash's Contact List App");
+        System.out.println("Welcome to Vibhu's Contact List App");
         System.out.println("Press 1 to add a new contact");
         System.out.println("Press 2 to view all contacts");
         System.out.println("Press 3 to search for a contact");
